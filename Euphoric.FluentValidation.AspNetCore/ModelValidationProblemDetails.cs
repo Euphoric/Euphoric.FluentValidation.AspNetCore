cb9e3e5215563be0ae56f7a655833aef1546f55c
Radek Falhar <Radek.Falhar@hotmail.com>
2022-10-31 15:18:58 +0100

    Ignore warning in ModelValidationProblemDetails

--- Euphoric.FluentValidation.AspNetCore/ModelValidationProblemDetails.cs
+++ Euphoric.FluentValidation.AspNetCore/ModelValidationProblemDetails.cs
@@ -4,5 +4,10 @@ namespace Euphoric.FluentValidation.AspNetCore;
 
 public class ModelValidationProblemDetails : ProblemDetails
 {
+#pragma warning disable CS8618
+    // ReSharper disable once UnusedAutoPropertyAccessor.Global
+    
     public IReadOnlyList<ModelValidationError> Errors { get; set; }
+    
+#pragma warning restore CS8618
 }
